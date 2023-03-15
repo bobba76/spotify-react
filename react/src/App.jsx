2368112bfae07c4b099f92f6e108af6160540b80
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-03-15 15:02:11 +0100

    added spotifywebapi

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+var SpotifyWebApi = import("spotify-web-api-node");
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId: "83b22e9984dd4eb191fa62637acaeffe",
+  clientSecret: "83cd765baebc4e238794797f04326166",
+  redirectUri: "https://localhost:5173",
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  spotifyApi
+    .getMySavedTracks({
+      limit: 1,
+      offset: 0,
+    })
+    .then(
+      function (data) {
+        console.log("Done!");
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
